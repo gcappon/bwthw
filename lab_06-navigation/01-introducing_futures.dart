25884d67c8ab3b2c9e1c10425364ac3b12edf80e
Giacomo Cappon <cappongiacomo@gmail.com>
2023-04-04 15:48:58 +0200

    [master] fixed typo on lesson 6

--- lab_06-navigation/01-introducing_futures.dart
+++ lab_06-navigation/01-introducing_futures.dart
@@ -1,16 +1,16 @@
 //An asyncronous function
-void fetchUserOrder() {
+Future<void> fetchUserOrder() async{
   // Imagine that this function is fetching user info from another service or database.
-  Future.delayed(const Duration(seconds: 2), () => print('Large Latte'));
+  await Future.delayed(const Duration(seconds: 2), () => print('Large Latte'));
 }//fetchUserOrder
 
 //Note that main is an asynchronous function now since it performs at least one asyncronous operation
-void main() {
+void main() async{
 
   print('Fetching user order...');
 
   //Calling the asynchronous function
-  fetchUserOrder();
+  await fetchUserOrder();
 
   //Note that the following print will be performed BEFORE the completion of the above function
   print('Done');
