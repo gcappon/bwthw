bb3e45e6db7b6308af377a23f7a495cd6015cd37
Giacomo Cappon <cappongiacomo@gmail.com>
2023-05-05 15:24:54 +0200

    Delete lab_08-networking directory

--- lab_08-networking/exercise_solutions/pokedex/android/app/src/main/kotlin/com/example/pokedex/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pokedex
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lab_08-networking/network_master/android/app/src/main/kotlin/com/example/network_master/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.network_master
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
