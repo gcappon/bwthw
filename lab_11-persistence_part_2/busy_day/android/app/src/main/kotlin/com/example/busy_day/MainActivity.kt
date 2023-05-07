cf06d978fe5a8199a85e16aa7cf446821ef4cdbb
Giacomo Cappon <cappongiacomo@gmail.com>
2023-05-07 16:58:54 +0200

    Delete lab_11-persistence_part_2 directory

--- lab_11-persistence_part_2/busy_day/android/app/src/main/kotlin/com/example/busy_day/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.busy_day
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lab_11-persistence_part_2/exercise_solutions/meal_manager/lib/utils/formats.dart
+++
@@ -1,9 +0,0 @@
-import 'package:intl/intl.dart';
-
-///Utility class that defines some custom [DateFormat].
-class Formats {
-  static final fullDateFormat = new DateFormat('KK:mm:ss aa, yyyy/MM/dd');
-  static final fullDateFormatNoSeconds = new DateFormat('KK:mm aa, yyyy/MM/dd');
-  static final onlyDayDateFormat = new DateFormat('yyyy/MM/dd');
-  static final onlyDayDateFormatTicks = new DateFormat('yyyy-MM-dd');
-} //Formats
