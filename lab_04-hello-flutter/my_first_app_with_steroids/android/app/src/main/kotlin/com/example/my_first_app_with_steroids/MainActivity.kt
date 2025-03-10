54f41452f98f693ce4b8a0f6bc3da2456ac6db99
Giacomo Cappon <cappongiacomo@gmail.com>
2025-03-10 15:18:11 +0100

    updated lab_04 to 24/25

--- lab_04-hello-flutter/my_first_app_with_steroids/android/app/src/main/kotlin/com/example/my_first_app_with_steroids/MainActivity.kt
+++ lab_04-hello-flutter/my_first_app_with_steroids/android/app/src/main/kotlin/com/example/my_first_app_with_steroids/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.my_first_app_with_steroids
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
