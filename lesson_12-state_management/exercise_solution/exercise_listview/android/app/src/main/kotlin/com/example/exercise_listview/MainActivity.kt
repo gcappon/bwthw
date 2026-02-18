760ebda4fd98b2fa4f599a6a7583d7b63c9dd394
Giacomo Cappon <cappongiacomo@gmail.com>
2026-02-18 14:47:34 +0100

    Delete lesson_12-state_management directory

--- lesson_12-state_management/exercise_solution/exercise_listview/android/app/src/main/kotlin/com/example/exercise_listview/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.exercise_listview
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lesson_12-state_management/exercise_solution/meal_manager/android/app/src/main/kotlin/com/example/meal_manager/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.meal_manager
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
